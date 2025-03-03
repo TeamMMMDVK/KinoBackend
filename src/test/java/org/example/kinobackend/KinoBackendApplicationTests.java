15bb05590d77f049f565170241f47c8872651380
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-03-03 11:51:09 +0100

    init

---
+++ src/test/java/org/example/kinobackend/KinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
