cbaaa6c0abbcd30869f9aa8f3206a2216a5d563e
MetteMunch <meha0005@stud.kea.dk>
2025-03-05 10:54:59 +0100

    Mette laver rod på dev

--- src/main/java/org/example/kinobackend/controller/MovieController.java
+++ src/main/java/org/example/kinobackend/controller/MovieController.java
@@ -1,9 +1,16 @@
 package org.example.kinobackend.controller;
 
+import org.example.kinobackend.model.Movie;
 import org.example.kinobackend.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -14,5 +21,20 @@ public class MovieController {
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
+
+    @GetMapping("/{id}")
+    public Movie showSpecificKommune(@PathVariable int id) {
+        try {
+            Optional<Movie> movieToBeReturned = movieService.getMovie(id);
+            return movieToBeReturned.get();
+        } catch (NoSuchElementException e) {
+            Movie noMovie = new Movie();
+            noMovie.setTitle("Movie not found");
+            return noKommune;
+        }
+
+    }
+
+
 
 }
--- src/main/java/org/example/kinobackend/service/MovieService.java
+++ src/main/java/org/example/kinobackend/service/MovieService.java
@@ -1,13 +1,20 @@
 package org.example.kinobackend.service;
 
+import org.example.kinobackend.model.Movie;
 import org.example.kinobackend.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
     public MovieService(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
+    }
+
+    public Optional<Movie> getMovie(int id) {
+       return movieRepository.findById(id);
     }
 }
