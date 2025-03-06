f450cd1d5a238623df6183b226fd9abf15f68263
MetteMunch <meha0005@stud.kea.dk>
2025-03-06 20:27:06 +0100

    Færdiggjort Endpoint til at finde bookede sæder på specifik show

--- src/main/java/org/example/kinobackend/controller/BookedSeatController.java
+++ src/main/java/org/example/kinobackend/controller/BookedSeatController.java
@@ -1,8 +1,14 @@
 package org.example.kinobackend.controller;
 
+import org.example.kinobackend.dto.BookedSeatDTO;
 import org.example.kinobackend.service.BookedSeatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/bookedseat")
@@ -12,6 +18,12 @@ public class BookedSeatController {
 
     public BookedSeatController(BookedSeatService bookedSeatService) {
         this.bookedSeatService = bookedSeatService;
+    }
+
+    @GetMapping("/show/{showID}")
+    public ResponseEntity<List<BookedSeatDTO>> getAllSeatsSpecificShowWithStatus(@PathVariable int showID) {
+        List<BookedSeatDTO> seatsOverviewSpecificShow = bookedSeatService.getAllSeatsSpecificShowWithStatus(showID);
+        return ResponseEntity.ok(seatsOverviewSpecificShow);
     }
 }
 
