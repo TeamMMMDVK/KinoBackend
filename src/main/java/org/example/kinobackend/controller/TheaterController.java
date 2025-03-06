a7a58cc6ea7788969cc0dc344a4d41f2cef4d09d
MetteMunch <meha0005@stud.kea.dk>
2025-03-06 08:19:19 +0100

    KINO-65 lavet Endpoint til at vise alle sæder i en specifik sal

--- src/main/java/org/example/kinobackend/controller/TheaterController.java
+++ src/main/java/org/example/kinobackend/controller/TheaterController.java
@@ -1,6 +1,10 @@
 package org.example.kinobackend.controller;
-
+import java.util.List;
+import org.example.kinobackend.model.Seat;
 import org.example.kinobackend.service.TheaterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,13 @@ public class TheaterController {
     public TheaterController(TheaterService theaterService) {
         this.theaterService = theaterService;
     }
+
+    @GetMapping("/{theaterId}/seats")
+    public ResponseEntity<List<Seat>> getSeatsByTheaterId(@PathVariable int theaterId) {
+        List<Seat> seats = theaterService.getSeatsByTheaterId(theaterId);
+        return ResponseEntity.ok(seats);
+
+    }
+
+
 }
--- src/main/java/org/example/kinobackend/service/TheaterService.java
+++ src/main/java/org/example/kinobackend/service/TheaterService.java
@@ -1,13 +1,30 @@
 package org.example.kinobackend.service;
 
+import org.example.kinobackend.model.Seat;
+import org.example.kinobackend.model.Theater;
 import org.example.kinobackend.repository.TheaterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TheaterService {
     private final TheaterRepository theaterRepository;
+
     public TheaterService(TheaterRepository theaterRepository) {
         this.theaterRepository = theaterRepository;
+    }
+
+    public List<Seat> getSeatsByTheaterId(int theaterID) {
+        Optional<Theater> optTheater = theaterRepository.findById(theaterID);
+        if (optTheater.isPresent()) {
+            return optTheater.get().getSeats();
+        } else {
+            return Collections.emptyList();
+        }
+
     }
 }
