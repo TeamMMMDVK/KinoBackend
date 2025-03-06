a45fc7526fba9862039f31ea4cf7cd9724661852
MetteMunch <meha0005@stud.kea.dk>
2025-03-06 16:54:01 +0100

    cherry picking

--- src/main/java/org/example/kinobackend/repository/BookedSeatRepository.java
+++ src/main/java/org/example/kinobackend/repository/BookedSeatRepository.java
@@ -3,12 +3,6 @@ package org.example.kinobackend.repository;
 import org.example.kinobackend.model.BookedSeat;
 import org.example.kinobackend.model.BookedSeatID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface BookedSeatRepository extends JpaRepository<BookedSeat, BookedSeatID> {
-
-    List<BookedSeat> findBookedSeatByShowMovieMovieID(int movieID); //finds all booked seats for a movie
 }
