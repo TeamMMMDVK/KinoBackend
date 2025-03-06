ccfa0deafc0b35d12b873047528418b37d8d556c
MetteMunch <meha0005@stud.kea.dk>
2025-03-06 12:14:26 +0100

    KINO-65 roder med at lave DTO, så PUT endpoint kan køre uden problemer JsonReferences..

--- src/main/java/org/example/kinobackend/controller/SeatController.java
+++ src/main/java/org/example/kinobackend/controller/SeatController.java
@@ -1,13 +1,11 @@
 package org.example.kinobackend.controller;
 
+import org.example.kinobackend.dto.SeatBlockUpdateDTO;
 import org.example.kinobackend.model.Seat;
 import org.example.kinobackend.repository.SeatRepository;
 import org.example.kinobackend.service.SeatService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/seat")
@@ -21,9 +19,17 @@ public class SeatController {
         this.seatRepository = seatRepository;
     }
 
-    @GetMapping("/{seatId}")
+    @GetMapping("/{seatId}") //Henter specifik sæde i en sal...ved ikke om denne skal bruges som Endpoint?
     public ResponseEntity<Seat> getSeatById(@PathVariable int seatId) {
         Seat requestedSeat = seatService.getSeatById(seatId);
         return ResponseEntity.ok(requestedSeat);
+    }
+
+    @PutMapping("/{seatId}")
+    public ResponseEntity<Seat> updateSeatBlockedStatus(@PathVariable int seatId, @RequestBody SeatBlockUpdateDTO seatBlockUpdateDTO) {
+        Seat seatToBeChanged = seatService.getSeatById(seatId);//Henter korrekt sæde i salen
+        seatToBeChanged.setBlocked(seatBlockUpdateDTO.isBlocked()); //ændrer attribut
+        Seat updatedSeat = seatService.updateSeat(seatToBeChanged); //gemmer ændringer i db
+        return ResponseEntity.ok(updatedSeat);
     }
 }
