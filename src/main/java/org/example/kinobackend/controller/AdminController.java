026510e07a4ad9756797a5657a9dece00e5679d5
ViAarisKea <viaa0001@stud.kea.dk>
2025-03-06 20:53:15 +0100

    KINO-6 add admin controller with the method for retrieving total number of tickets for a specific movie

---
+++ src/main/java/org/example/kinobackend/controller/AdminController.java
@@ -0,0 +1,31 @@
+package org.example.kinobackend.controller;
+
+import org.example.kinobackend.model.UserRole;
+import org.example.kinobackend.service.AdminService;
+import org.example.kinobackend.service.SecurityService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+    private final SecurityService securityService;
+
+    public AdminController(AdminService adminService, SecurityService securityService) {
+        this.adminService = adminService;
+        this.securityService = securityService;
+    }
+
+    @GetMapping("/movie/{id}/sale")                         //get the role from the header of the request
+    public ResponseEntity<?> getTotalNumberOfTickets(@RequestHeader(value = "Role") String role,
+                                                     @PathVariable int id) {
+        if (securityService.hasRole(role, UserRole.ADMIN)) {
+            return ResponseEntity.ok(adminService.getTotalNumberOfTickets(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access for role " + role + " denied.");
+        }
+    }
+}
