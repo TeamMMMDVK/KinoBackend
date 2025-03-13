5c42128f24d03826edadb5584551961a62523cd5
DanielJensenKEA <daje0003@stud.kea.dk>
2025-03-13 09:25:58 +0100

    @Lob på Image attribut

--- src/main/java/org/example/kinobackend/model/Image.java
+++ src/main/java/org/example/kinobackend/model/Image.java
@@ -12,6 +12,7 @@ public class Image {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int imageID;
     private String name;
+    @Lob
     private String image; //Image gemmes og transporteres som 64bit Streng eller noget i den stil...
     private LocalDate saved;
 
